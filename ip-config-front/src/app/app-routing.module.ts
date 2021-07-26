9b1835a56c595362dcc7424d8360895dfabcbe26
hobingarit <dominating.kez@gmail.com>
2021-07-27 02:12:37 +0800

    update hingga tampil halaman edit

--- ip-config-front/src/app/app-routing.module.ts
+++ ip-config-front/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { AboutComponent } from './components/about/about.component'; 
+import { IpsComponent } from './components/ips/ips.component';
+const routes: Routes = [
+  {path: '', component: IpsComponent},
+  {path: 'about', component: AboutComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- ip-config-front/src/app/app.module.ts
+++ ip-config-front/src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ButtonComponent } from './components/button/button.component';
+import { IpsComponent } from './components/ips/ips.component';
+import { IpItemComponent } from './components/ip-item/ip-item.component';
+import { AddIpComponent } from './components/add-ip/add-ip.component';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ButtonComponent,
+    IpsComponent,
+    IpItemComponent,
+    AddIpComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
